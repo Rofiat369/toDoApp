ef3146bcb638b14fb1e882ebccc0844bc557ea1f
Rofiat <rofiatolusanya12@gmail.com>
2022-05-07 19:31:03 -0400

    created components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: FunctionComponent = () => {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField />
     </div>
   );
 }
