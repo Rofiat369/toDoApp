a3f2f14f826544d1d81e813ef790c7a2cea77067
Rofiat <rofiatolusanya12@gmail.com>
2022-05-07 22:05:20 -0400

    Created and added components to App folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React, { FunctionComponent, useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import ToDoList from './components/ToDoList';
+import { ToDos } from './model';
 
 const App: FunctionComponent = () => {
 const [toDo, setToDo] = useState<string>("")
-console.log(toDo);
+const [toDos, setToDos] = useState<ToDos[]>([]);
+
+const handleAdd = (event: React.FormEvent)=>{
+  event.preventDefault();
+
+  if(toDo){
+    setToDos([...toDos, {id: Date.now(), todo: toDo, isDone: false}])
+    setToDo("")
+  }
+}
+
+console.log(toDos);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField toDo={toDo} setToDo={setToDo} />
+      <InputField toDo={toDo} setToDo={setToDo} handleAdd={handleAdd} />
+      <ToDoList todos={toDos} setTodos={setToDos} />
     </div>
   );
 }
