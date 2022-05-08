c2b8797794f705995c725c04ce71bb89d77c39e8
Rofiat <rofiatolusanya12@gmail.com>
2022-05-07 20:09:50 -0400

    Added state to react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 const App: FunctionComponent = () => {
+const [toDo, setToDo] = useState<string>("")
+console.log(toDo);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField toDo={toDo} setToDo={setToDo} />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface InputFieldProps{
+    toDo: string;
+    setToDo:React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: FunctionComponent<InputFieldProps> = ({toDo, setToDo}) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input type="input"
+      value={toDo}
+      onChange={(event) => setToDo(event.target.value)}
+       placeholder="Enter a task" 
+       className="input__box" />
       <button className="input__submit" type="submit">
         Go
       </button>
